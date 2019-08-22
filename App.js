fa77209f89ad74ccbc03944a49cc2c35b522cf3b
harsh2201 <40879349+harsh2201@users.noreply.github.com>
2019-08-22 23:28:05 +0530

    Initial config with drawer

--- App.js
+++ App.js
@@ -1,12 +1,111 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { View, Dimensions, StyleSheet, Text } from "react-native";
+import { BottomNavigation, Paragraph } from "react-native-paper";
 
 export default class App extends React.Component {
+  state = {
+    index: 0,
+    routes: [
+      {
+        key: "home",
+        title: "Home",
+        icon: "home"
+        // color: "#039b3b"
+      },
+      {
+        key: "favorites",
+        title: "Favorites",
+        icon: "favorite"
+        // color: "#E81B38"
+      },
+      {
+        key: "profile",
+        title: "Profile",
+        icon: "person"
+        // color: "#F75728"
+      },
+      {
+        key: "aboutUs",
+        title: "About Us",
+        icon: "info"
+        // color: "#000"
+      }
+    ]
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <BottomNavigation
+        navigationState={this.state}
+        onIndexChange={index => this.setState({ index })}
+        renderScene={BottomNavigation.SceneMap({
+          home: () => {
+            return (
+              <View style={styles.content}>
+                <Text
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    textAlign: "center",
+                    fontSize: 30
+                  }}
+                >
+                  Home
+                </Text>
+              </View>
+            );
+          },
+          favorites: () => {
+            return (
+              <View style={[styles.content]}>
+                <Text
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    textAlign: "center",
+                    fontSize: 30
+                  }}
+                >
+                  Favourites
+                </Text>
+              </View>
+            );
+          },
+          profile: () => {
+            return (
+              <View style={[styles.content]}>
+                <Text
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    textAlign: "center",
+                    fontSize: 30
+                  }}
+                >
+                  Profile
+                </Text>
+              </View>
+            );
+          },
+          aboutUs: () => {
+            return (
+              <View style={[styles.content, { backgroundColor: "#000" }]}>
+                <Text
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    textAlign: "center",
+                    fontSize: 30,
+                    color: "#fff"
+                  }}
+                >
+                  About Us
+                </Text>
+              </View>
+            );
+          }
+        })}
+      />
     );
   }
 }
@@ -14,8 +113,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });

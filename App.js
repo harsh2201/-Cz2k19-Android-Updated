a86c6afbd70e8d8ac8d693e2b5c52119578ab915
harsh2201 <40879349+harsh2201@users.noreply.github.com>
2019-08-22 23:35:02 +0530

    Minor file changes

--- App.js
+++ App.js
@@ -1,34 +1,31 @@
 import * as React from "react";
 import { View, Dimensions, StyleSheet, Text } from "react-native";
 import { BottomNavigation, Paragraph } from "react-native-paper";
+import Events from "./Components/Events";
+import Options from "./Components/Options";
+import About from "./Components/About";
 
 export default class App extends React.Component {
   state = {
     index: 0,
     routes: [
       {
-        key: "home",
-        title: "Home",
+        key: "events",
+        title: "Events",
         icon: "home"
         // color: "#039b3b"
       },
       {
-        key: "favorites",
-        title: "Favorites",
+        key: "options",
+        title: "Options",
         icon: "favorite"
         // color: "#E81B38"
       },
       {
-        key: "profile",
-        title: "Profile",
+        key: "about",
+        title: "About CZ",
         icon: "person"
         // color: "#F75728"
-      },
-      {
-        key: "aboutUs",
-        title: "About Us",
-        icon: "info"
-        // color: "#000"
       }
     ]
   };
@@ -39,71 +36,9 @@ export default class App extends React.Component {
         navigationState={this.state}
         onIndexChange={index => this.setState({ index })}
         renderScene={BottomNavigation.SceneMap({
-          home: () => {
-            return (
-              <View style={styles.content}>
-                <Text
-                  style={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    textAlign: "center",
-                    fontSize: 30
-                  }}
-                >
-                  Home
-                </Text>
-              </View>
-            );
-          },
-          favorites: () => {
-            return (
-              <View style={[styles.content]}>
-                <Text
-                  style={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    textAlign: "center",
-                    fontSize: 30
-                  }}
-                >
-                  Favourites
-                </Text>
-              </View>
-            );
-          },
-          profile: () => {
-            return (
-              <View style={[styles.content]}>
-                <Text
-                  style={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    textAlign: "center",
-                    fontSize: 30
-                  }}
-                >
-                  Profile
-                </Text>
-              </View>
-            );
-          },
-          aboutUs: () => {
-            return (
-              <View style={[styles.content, { backgroundColor: "#000" }]}>
-                <Text
-                  style={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    textAlign: "center",
-                    fontSize: 30,
-                    color: "#fff"
-                  }}
-                >
-                  About Us
-                </Text>
-              </View>
-            );
-          }
+          events: Events,
+          options: Options,
+          about: About
         })}
       />
     );

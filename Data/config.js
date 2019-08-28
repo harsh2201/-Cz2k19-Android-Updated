f83ada51f448b2bdb73abc2d2e86e7ee81056bc9
satvikpadhiyar <slpadhiyar@gmail.com>
2019-08-28 19:47:30 +0530

    database api key changed

--- Data/config.js
+++ Data/config.js
@@ -1,9 +1,18 @@
+// export default firebaseConfig = {
+//   apiKey: "AIzaSyCpsdNSarpuc8Cb3GHcHjbPYvfBeim2JkY",
+//   authDomain: "cognizance2k19-169d7.firebaseapp.com",
+//   databaseURL: "https://cognizance2k19-169d7.firebaseio.com",
+//   projectId: "cognizance2k19-169d7",
+//   storageBucket: "",
+//   messagingSenderId: "656512761398",
+//   appId: "1:656512761398:web:d5221d4e8653cd22"
+// };
 export default firebaseConfig = {
-  apiKey: "AIzaSyCpsdNSarpuc8Cb3GHcHjbPYvfBeim2JkY",
-  authDomain: "cognizance2k19-169d7.firebaseapp.com",
-  databaseURL: "https://cognizance2k19-169d7.firebaseio.com",
-  projectId: "cognizance2k19-169d7",
+  apiKey: "AIzaSyCvRSKpvSZV8nZ84cu32xs-fBijxQLtoMc",
+  authDomain: "cz-ios-test.firebaseapp.com",
+  databaseURL: "https://cz-ios-test.firebaseio.com",
+  projectId: "cz-ios-test",
   storageBucket: "",
-  messagingSenderId: "656512761398",
-  appId: "1:656512761398:web:d5221d4e8653cd22"
+  messagingSenderId: "612864123197",
+  appId: "1:612864123197:web:a51986025017f080"
 };
